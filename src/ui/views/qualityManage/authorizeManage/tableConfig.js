f956d2091fae7bcb475ba21468c3bfd8b314afed
liuping <271578727@qq.com>
2020-08-07 10:53:35 +0800

    jiekou

--- src/ui/views/qualityManage/authorizeManage/tableConfig.js
+++ src/ui/views/qualityManage/authorizeManage/tableConfig.js
@@ -29,7 +29,7 @@ export const authorizeConfig = (obj,arr) => {
             { prop: 'post', label: '岗位',sortProp:"post", align: 'center',sort:true,search:{type:'input', prop:'post',placeholder:"请输入"} },
             { prop: 'authorizationType', label: '授权类型',sortProp:"authorizationType", align: 'center',sort:true,search:{type:'select', prop:'authorizationTypeQuery',placeholder:"请选择",data:obj.roleControl,selectProp:['valData','valData']} },
             { prop: 'authorizedUnit', label: '授权单位',sortProp:"authorizedUnit", align: 'center',sort:true,search:{type:'input', prop:'authorizedUnit',placeholder:"请输入"} },
-            { prop: 'flightType', label: '授权航班类型',sortProp:"flightType", align: 'center',sort:true,search:{type:'select', prop:'flightType ',placeholder:"请选择",data:obj.accreditFlightType,selectProp:['valData','valData']} },
+            { prop: 'flightType', label: '授权航班类型',sortProp:"flightType", align: 'center',sort:true,search:{type:'select', prop:'flightType',placeholder:"请选择",data:obj.accreditFlightType,selectProp:['valData','valData']} },
             { prop: 'modelRange', label: '授权机型',formatter:modelRange,sortProp:"modelRange", align: 'center',sort:true,search:{type:'select', prop:'modelRange',placeholder:"请选择",data:arr,selectProp:['name','id']} },
             { prop: 'state', label: '授权状态',formatter:state,sortProp:"state", align: 'center',sort:true,search:{type:'select', prop:'state',placeholder:"请选择",data:obj.accreditState,selectProp:['valData','valCode']} },
              { slot: 'option', label: '关联信息查看' ,width:'230',search:{fixed:"right",type:'btn',label:'搜索',icon:"table_search"}}
