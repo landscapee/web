78a2d04fa5148fdefe07a1af5bd2245a689b162a
liuping <271578727@qq.com>
2020-08-26 09:39:44 +0800

    完工管控

---
+++ src/ui/views/workOrder/signControl/component/info.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <div class="DivBox">
+            <div class="item">
+                <div class="left">任务日期：</div>
+                <div class="right">{{form.workTime? moment(form.workTime).format('YYYY-MM-DD'):'--'}}</div>
+            </div>
+            <div class="item">
+                <div class="left">任务号：</div>
+                <div class="right">{{form.ommsJobNo}}</div>
+            </div>
+            <div class="item">
+                <div class="left">工单编号：</div>
+                <div class="right">{{form.serialNo}}</div>
+            </div>
+            <div class="item">
+                <div class="left">工单模板编码：</div>
+                <div class="right">{{get(form.template,'code')}}</div>
+            </div>
+            <div class="item">
+                <div class="left">模板名称：</div>
+                <div class="right">{{get(form.template,'title')}}</div>
+            </div><div class="item">
+            <div class="left">模板版本号：</div>
+            <div class="right">{{get(form.template,'version')}}werwerwerwerwerwerew</div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import moment from 'moment'
+
+    import {map,get} from 'lodash'
+    export default {
+        name: "info",
+        props:['form'],
+        components: {},
+        data() {
+            return {
+                get:get,
+                moment:moment
+            }
+        },
+        methods: {},
+        created() {
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    .DivBox {
+        .item{
+            display: flex;
+            height:30px;
+            justify-content: left;
+            align-items: center;
+            .left{
+                width:140px;
+            }
+            .right{
+                width:calc(100% - 140px)
+            }
+        }
+    }
+</style>
--- src/ui/views/workOrder/signControl/tableConfig.js
+++ src/ui/views/workOrder/signControl/tableConfig.js
@@ -1,22 +1,75 @@
+import moment from 'moment'
+import {map} from 'lodash'
 
 
+let  timeInfo1=(row)=>{
+    if(row.workTime){
+        return moment(row.workTime).format('YYYY-MM-DD')
+    }else {
+        return ''
+    }
+}
 
+let obj1={
+    0:'待执行',
+    1:'上报中',
+    2:'作业中',
+    3:'完成',
+    4:'无效',
+}
+let arrstate=[
+    {valData:'待执行',valCode:0},
+    {valData:'上报中',valCode:1},
+    {valData:'作业中',valCode:2},
+    {valData:'完成',valCode:3},
+    {valData:'无效',valCode:4},
+ ]
+let state=(row)=>{
 
+    return obj1[row.state]
+}
+let etopEnable=(row)=>{
+    if(row.template.etopEnable===true){
+        return '适用'
+    }else if(row.template.etopEnable===false){
+        return '不适用'
+    }else{
+        return ''
+    }
 
-export const authorizeConfig = (obj,arr) => {
+}
+let worldorderTypeObj={}
+
+export const  Config = (obj,arr,arr1,arr2,arr3) => {
+    map(obj.worldorderType,(k,l)=>{
+        worldorderTypeObj[k.valCode]=k.valData
+    })
+    let worktype=(row)=>{
+        if(row.template&&row.template.type){
+            return worldorderTypeObj[row.template.type]
+        }else{
+            return ''
+        }
+    }
     return [
-        { slot: 'radio' , label: '选择',width:49,search:{type:'text',label:'过滤'}},
-        { prop: 'userNumber', label: '工单模板版本号',sortProp:"userNumber", align: 'center',sort:false },
-        { prop: 'userName', label: '最新版本标识',sortProp:"userName", align: 'center',sort:false,search:{type:'input', prop:'userName',placeholder:"请输入"} },
-        { prop: 'dept', label: '工单模板编码',sortProp:"dept", align: 'center',sort:true,search:{type:'input', prop:'dept',placeholder:"请输入"} },
-        { prop: 'post', label: '工单标题',sortProp:"post", align: 'center',sort:true,search:{type:'input', prop:'post',placeholder:"请输入"} },
-        { prop: 'authorizationType', label: '模板启用日期',sortProp:"authorizationType", align: 'center',sort:true,search:{type:'date', prop:'authorizationType',placeholder:"请选择", } },
-        { prop: 'authorizedUnit', label: '模板编制人',sortProp:"authorizedUnit", align: 'center',sort:true,search:{type:'input', prop:'authorizedUnit',placeholder:"请输入"} },
-        { prop: 'flightType', label: '版本发布日期',sortProp:"flightType", align: 'center',sort:true,search:{type:'date', prop:'flightType',placeholder:"请选择" } },
-        { prop: 'flightType', label: '版本生效日期',sortProp:"flightType", align: 'center',sort:true,search:{type:'date', prop:'flightType',placeholder:"请选择" } },
-        { prop: 'flightType', label: '版本失效日期',sortProp:"flightType", align: 'center',sort:true,search:{type:'date', prop:'flightType',placeholder:"请选择" } },
-        { prop: 'modelRange', label: '版本状态', sortProp:"modelRange", align: 'center',sort:true,search:{type:'select', prop:'modelRange',placeholder:"请选择",data:arr,selectProp:['name','id']} },
-        { slot: 'option', label: '操作' ,width:'160',search:{fixed:"right",type:'btn',label:'搜索',icon:"table_search"}}
+        { slot: 'checkbox' , label: '选择',width:49,search:{type:'text',label:'过滤'}},
+        { prop: 'workTime', label: '任务日期',formatter:timeInfo1, sortProp:"workTime", align: 'center',sort:true ,search:{type:'date', prop:'workTime',placeholder:"请选择"} },
+        { prop: 'ommsJobNo', label: '任务号',sortProp:"ommsJobNo", align: 'center',sort:true,search:{type:'input', prop:'ommsJobNo',placeholder:"请输入"} },
+        { prop: 'serialNo', label: '工单流水号',sortProp:"serialNo", align: 'center',sort:true,search:{type:'input', prop:'serialNo',placeholder:"请输入"} },
+
+        { prop: 'template.code', label: '工单模板编码',sortProp:"code", align: 'center',sort:true,search:{type:'input', prop:'code',placeholder:"请输入"} },
+        { prop: 'template.title', label: '工单名称',sortProp:"title", align: 'center',sort:true,search:{type:'input', prop:'title',placeholder:"请输入"} },
+         { prop: 'template.type', formatter:worktype,label: '工单类型',sortProp:"type", align: 'center',sort:true,search:{type:'select', prop:'type',placeholder:"请输入",data:obj.worldorderType,selectProp:['valData','valCode'] } },
+
+        { prop: 'airlineCompanyCode', label: '航司代码',sortProp:"airlineCompanyCode", align: 'center',sort:true,search:{type:'input', prop:'airlineCompanyCode',placeholder:"请输入",data:arr,selectProp:['name','id'] } },
+        { prop: 'airlineCompanyName', label: '航司名称',sortProp:"airlineCompanyName", align: 'center',sort:true,search:{type:'input', prop:'airlineCompanyName',placeholder:"请输入" ,data:arr1,selectProp:['name','id']} },
+        { prop: 'airplaneIcao', label: '机型',sortProp:"airplaneIcao", align: 'center',sort:true,search:{type:'input', prop:'airplaneIcao',placeholder:"请输入",data:arr2,selectProp:['name','id'] } },
+        { prop: 'flightState', label: '航班类型', sortProp:"flightState", align: 'center',sort:true,search:{type:'select', prop:'flightState',placeholder:"请选择",data:obj.W_flightType,selectProp:['valData','valCode']} },
+        { prop: 'template.jobType', label: '作业类型', sortProp:"jobType", align: 'center',sort:true,search:{type:'select', prop:'jobType',placeholder:"请选择",data:obj.W_workType,selectProp:['valData','valCode']} },
+        { prop: 'template.personType', label: '作业员类型', sortProp:"personType", align: 'center',sort:true,search:{type:'select', prop:'personType',placeholder:"请选择",data:obj.workUserType,selectProp:['valData','valCode']} },
+        { prop: 'template.etopEnable', formatter:etopEnable,label: '是否适用ETOPS运行', sortProp:"etopEnable", align: 'center',sort:true,search:{type:'select', prop:'etopEnable',placeholder:"请选择",data:obj.applyETOP,selectProp:['valData','valCode']} },
+        { prop: 'state', label: '工单状态',formatter:state, sortProp:"state", align: 'center',sort:true,search:{type:'select', prop:'state',placeholder:"请选择",data:arrstate,selectProp:['valData','valCode']} },
+        { slot: 'option', label: '操作' ,width:'80',search:{fixed:"right",type:'btn',label:'搜索',icon:"table_search"}}
     ]
 };
 
